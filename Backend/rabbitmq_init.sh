acf4489787e07c10d107f3a6f9714c9a5dae31f9
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-06-19 20:58:50 +0300

    RabbitMQ now correctly adds user accounts on startup

--- Backend/rabbitmq_init.sh
+++ Backend/rabbitmq_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rabbitmq-server
+rabbitmq-server -detached
 sleep 20
 
 rabbitmqctl add_user authentication Password123!
@@ -8,3 +8,7 @@ rabbitmqctl set_permissions -p / authentication ".*" ".*" ".*"
 
 rabbitmqctl add_user course_management Password123!
 rabbitmqctl set_permissions -p / course_management ".*" ".*" ".*"
+
+rabbitmqctl stop
+sleep 20
+rabbitmq-server
