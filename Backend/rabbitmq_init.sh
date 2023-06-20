e93ca0a57a7f5b00e42e5faed026a58ada762a56
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-06-20 17:30:56 +0300

    Modified docker-compose in order to enable .NET hot reloading

--- Backend/rabbitmq_init.sh
+++ Backend/rabbitmq_init.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+set -e
 
+# Start RabbitMQ server in the background
 rabbitmq-server -detached
+
 sleep 20
 
-rabbitmqctl add_user authentication Password123!
-rabbitmqctl set_permissions -p / authentication ".*" ".*" ".*"
+# Check if the 'authentication' user exists
+if ! rabbitmqctl list_users | grep -q '^authentication'; then
+    # If not, add user and set permissions
+    rabbitmqctl add_user authentication Password123!
+    rabbitmqctl set_permissions -p / authentication ".*" ".*" ".*"
+fi
 
-rabbitmqctl add_user course_management Password123!
-rabbitmqctl set_permissions -p / course_management ".*" ".*" ".*"
+# Check if the 'course_management' user exists
+if ! rabbitmqctl list_users | grep -q '^course_management'; then
+    # If not, add user and set permissions
+    rabbitmqctl add_user course_management Password123!
+    rabbitmqctl set_permissions -p / course_management ".*" ".*" ".*"
+fi
 
+# Stop the background RabbitMQ server
 rabbitmqctl stop
-sleep 20
+
+# Start RabbitMQ server in the foreground
 rabbitmq-server
